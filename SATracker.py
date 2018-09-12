b3cef16675b3384c0ad448e59fc92d260cbbf372
greatdsa <30824645+greatdsa@users.noreply.github.com>
2018-09-12 22:29:26 +0200

    Update SATracker.py
    Changed the threads into processes. Trying to get the timestamp

--- SATracker.py
+++ SATracker.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import tobii_research as tr
 import time
 import math
@@ -20,12 +21,10 @@ def gaze_data_callback(gaze_data):
         gaze_point = ((left_3d), (right_3d))
         gaze_point = tuple(mean(gaze_point, axis=0))
         print("3d gaze:",gaze_point)
-        return [gaze_point]
 
-#Starting thread
-for x in range(1,2):
- t = threading.Thread(target=gaze_data_callback(gaze_data), args=(x,))
- t.start()
+#Get the timestamp
+        timestamp = tr.get_system_time_stamp()
+        return [gaze_point]
 
  #Working on 2D data
 def data_collection():
@@ -37,3 +36,11 @@ def data_collection():
                 x = data[0] * 1920
                 y = data[1] * 1080
                 return [x, y]
+
+
+#Multiprocessing
+if __name__ == "__main__":
+    p1 = multiprocessing.Process(name="p1", target=gaze_data_callback)
+    p2 = multiprocessing.Process(name="p2", target=data_collection)
+    p1.start()
+    p2.start()
