f734f9707622df82c75d35554a01f0ccf03abe6d
greatdsa <30824645+greatdsa@users.noreply.github.com>
2018-09-14 15:01:59 +0200

    Update mean
    Implemented manual mean

--- SATracker.py
+++ SATracker.py
@@ -1,9 +1,8 @@
+from statistics import mean
 import multiprocessing
 import tobii_research as tr
 import time
 import math
-from numpy import mean
-import threading
 
 #Eye Tracker Initialization
 found_eyetrackers = tr.find_all_eyetrackers()
@@ -18,29 +17,54 @@ def gaze_data_callback(gaze_data):
         left_3d = gaze_data['left_gaze_point_in_user_coordinate_system']
         right_3d = gaze_data['right_gaze_point_in_user_coordinate_system']
 #Get the gaze point of both eyes
-        gaze_point = ((left_3d), (right_3d))
-        gaze_point = tuple(mean(gaze_point, axis=0))
-        print("3d gaze:",gaze_point)
+        print('L3d:', left_3d)
+        print('R3d:', right_3d)
+        numbers = [0, 0, 0]
+        numbers[0] = [left_3d[0], right_3d[0]]
+        numbers[1] = [left_3d[1], right_3d[1]]
+        numbers[2] = [left_3d[2], right_3d[2]]
+        #print(numbers)
+        x = [0, 0, 0]
+        x[0] = sum(numbers[0])
+        x[1] = sum(numbers[1])
+        x[2] = sum(numbers[2])
+        x[0] = x[0]/2
+        x[1] = x[1]/2
+        x[2] = x[2]/2
+        print(x)
+
+        #gaze_point = ((left_3d), (right_3d))
+        #gaze_point = tuple(np.mean(gaze_point, axis=0))
+        #gaze_point = tuple(mean(gaze_point, axis=0))
+        #print("3d gaze:",gaze_point)
 
 #Get the timestamp
         timestamp = tr.get_system_time_stamp()
-        return [gaze_point]
+        print(timestamp)
+        #return [x]
 
  #Working on 2D data
-def data_collection():
-    while running:
-        if 'gaze_point' in gaze_data_callback():
-            data = gaze_data_callback['gaze_point']
+def test():
+    a=0
+    while a<3:
+        #if 'gaze_point' in gaze_data_callback(gaze_data):
+         #   data = gaze_data_callback['gaze_point']
 
-            if s == 0:
-                x = data[0] * 1920
-                y = data[1] * 1080
-                return [x, y]
+          #  if s == 0:
+           #     x = data[0] * 1920
+            #    y = data[1] * 1080
+             #   return [x, y]
+        a+=1
+        for x in range(0, 10):
+            print("We're on time %d" % (x))
+        print('Waiting..')
+        time.sleep(5)
+
 
 
 #Multiprocessing
 if __name__ == "__main__":
     p1 = multiprocessing.Process(name="p1", target=gaze_data_callback)
-    p2 = multiprocessing.Process(name="p2", target=data_collection)
+    p2 = multiprocessing.Process(name="p2", target=test)
     p1.start()
     p2.start()
