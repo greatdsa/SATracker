987cec5eba92193393ddc42ccade754cbe35c732
greatdsa <30824645+greatdsa@users.noreply.github.com>
2018-09-21 14:59:15 +0200

    Update SATracker.py
    This isn#t a working code. Just to save it.

--- SATracker.py
+++ SATracker.py
@@ -7,6 +7,7 @@ import threading
 found_eyetrackers = tr.find_all_eyetrackers()
 system_time_stamp = tr.get_system_time_stamp()
 print("The system time stamp in microseconds is {0}.".format(system_time_stamp))
+print(tr.EYETRACKER_GAZE_DATA)
 my_eyetracker = found_eyetrackers[0]
 print("Address: " + my_eyetracker.address)
 print("Model: " + my_eyetracker.model)
@@ -16,17 +17,25 @@ print("Serial number: " + my_eyetracker.serial_number)
 
 # Getting Gaze Point
 def gaze_data_callback(gaze_data):
+        print(type(gaze_data))
+
+
         left_3d = gaze_data['left_gaze_point_in_user_coordinate_system']
         right_3d = gaze_data['right_gaze_point_in_user_coordinate_system']
+        gaze = {'left': left_3d , 'right': right_3d}
+        #print(gaze)
+        print(type(gaze))
         gaze_point = (left_3d, right_3d)
         gaze_point = tuple(mean(gaze_point,axis=0))
         print("3d gaze point:",gaze_point)
         time.sleep(3)
+        return gaze
+
 
 # Calling Gaze Function with timestamp
 def call_gaze():
     my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
-
+    print(a)
     while 1:
         print(tr.get_system_time_stamp())
         time.sleep(2)
@@ -43,7 +52,7 @@ def test():
         #time.sleep(5)
 
 
-#Multithreading
+# Multithreading
 def main():
     thread1 = threading.Thread(target=call_gaze)
     thread2 = threading.Thread(target=test)
@@ -54,5 +63,8 @@ def main():
     thread1.join()
     thread2.join()
 
+
 if __name__ == "__main__":
+        gaze_point_2d = []
+        gaze_point_2D_list = []
         main()
