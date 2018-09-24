8291d87e07864db242de4105e1ce6a95daa28b2d
greatdsa <30824645+greatdsa@users.noreply.github.com>
2018-09-24 16:06:05 +0200

    Corrected Gaze position
    Now, getting 2d gaze position, system timestamp and device timestamp.
    Also getting the gaze position outside callback using global variable.

--- SATracker.py
+++ SATracker.py
@@ -3,10 +3,16 @@ import time
 from numpy import mean
 import threading
 
+
+# Global Variables
+gaze_position = [0, 0]
+timestamp = 0
+
 # Eye Tracker Initialization
 found_eyetrackers = tr.find_all_eyetrackers()
 system_time_stamp = tr.get_system_time_stamp()
 print("The system time stamp in microseconds is {0}.".format(system_time_stamp))
+print(tr.EYETRACKER_GAZE_DATA)
 my_eyetracker = found_eyetrackers[0]
 print("Address: " + my_eyetracker.address)
 print("Model: " + my_eyetracker.model)
@@ -16,43 +22,53 @@ print("Serial number: " + my_eyetracker.serial_number)
 
 # Getting Gaze Point
 def gaze_data_callback(gaze_data):
-        left_3d = gaze_data['left_gaze_point_in_user_coordinate_system']
-        right_3d = gaze_data['right_gaze_point_in_user_coordinate_system']
-        gaze_point = (left_3d, right_3d)
-        gaze_point = tuple(mean(gaze_point,axis=0))
-        print("3d gaze point:",gaze_point)
+        global gaze_position
+        global timestamp
+        left_eye = gaze_data['left_gaze_point_on_display_area']
+        right_eye = gaze_data['right_gaze_point_on_display_area']
+        gaze_position = (left_eye,right_eye)
+        print('Without mean:', gaze_position)
+        gaze_position = tuple(mean(gaze_position,axis=0))
+        timestamp = gaze_data['device_time_stamp']
+        timestamp = timestamp/1000000
         time.sleep(3)
+
 
 # Calling Gaze Function with timestamp
 def call_gaze():
     my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
 
     while 1:
-        print(tr.get_system_time_stamp())
+        print(gaze_position)
+        print(timestamp)
+        ts = tr.get_system_time_stamp()/1000000
+        print(ts)
         time.sleep(2)
 
 
 # Random Test Function with the Timestamp
 def test():
-     while 1:
+    while 1:
         for x in range(0, 10):
             print("We're on time %d" % (x))
             time.sleep(2)
         print('Waiting..')
-        print(tr.get_system_time_stamp())
+        print(timestamp)
+        ts = tr.get_system_time_stamp()/1000000
+        print(ts)
         #time.sleep(5)
 
 
-#Multithreading
+# Multi-threading
 def main():
     thread1 = threading.Thread(target=call_gaze)
     thread2 = threading.Thread(target=test)
     # Will execute both in parallel
     thread1.start()
     thread2.start()
-    # Joins threads back to the parent process, which is this program
-    thread1.join()
-    thread2.join()
+
 
 if __name__ == "__main__":
+        gaze_position_2D = ()
+        gaze_position_2D_tuple = ()
         main()
