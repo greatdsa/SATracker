c502817b419f41cc2e4e6055d70896c82f6d2c29
greatdsa <30824645+greatdsa@users.noreply.github.com>
2018-09-26 18:07:06 +0200

    Interpolation
    Interpolation of one point

--- SATracker.py
+++ SATracker.py
@@ -4,10 +4,12 @@ from numpy import mean
 import threading
 import math
 
+# Global Variables
 gaze_position = (0, 0)
 timestamp = 0
 #gaze_position_2D_tuple = (gaze_position,)
 Sample_point_tuple = tuple()
+interpolated_sample_point = tuple()
 
 # Eye Tracker Initialization
 found_eyetrackers = tr.find_all_eyetrackers()
@@ -27,13 +29,13 @@ def gaze_data_callback(gaze_data):
         global timestamp
         left_eye = gaze_data['left_gaze_point_on_display_area']
         right_eye = gaze_data['right_gaze_point_on_display_area']
-        print('Left:',left_eye)
-        print('Right:',right_eye)
+        #print('Left:',left_eye)
+        #print('Right:',right_eye)
         gaze_position = (left_eye, right_eye)
         gaze_position = tuple(mean(gaze_position, axis=0))
         print('With mean:', gaze_position)
         timestamp = gaze_data['device_time_stamp']
-        timestamp = timestamp/1000
+        timestamp = timestamp/1000000
 
 
 # Calling Gaze Function with timestamp
@@ -62,10 +64,10 @@ def sp():
     while 1:
         time.sleep(0.01111111)
         if not (math.isnan(gaze_position[0]) and math.isnan(gaze_position[1])):
-            Sample_point = (gaze_position[0],gaze_position[1], timestamp)
+            Sample_point = (gaze_position[0], gaze_position[1], timestamp)
             Sample_point_tuple = Sample_point_tuple + (Sample_point,)
             print('Sample points:', Sample_point_tuple)
-
+          #  print(len(Sample_point_tuple))
           #  sample_point_list = list(Sample_point_tuple)
           #  print(sample_point_list)
 
@@ -79,6 +81,46 @@ def sp():
          #           del sample_point_list[count]
            # Sample_point_list = tuple(Sample_point_tuple)
           #  print('Sample Points R:', sample_point_list)
+
+
+# Gap Filling Function
+def interpolation():
+    global Sample_point_tuple
+    global interpolated_sample_point
+    time_parameter = 0.075
+    time.sleep(1)
+    while 1:
+        number_of_ts = len(Sample_point_tuple)
+        if number_of_ts > 1:
+            for i in range(0, number_of_ts):
+                delta_ts = Sample_point_tuple[i][2] - Sample_point_tuple[i-1][2]
+                if delta_ts <= time_parameter:
+                    interpolated_sample_point = interpolated_sample_point + Sample_point_tuple[i]
+                    #print(time_parameter)
+                else:
+                    ts = delta_ts/time_parameter
+                    x = Sample_point_tuple[i][0] - Sample_point_tuple[i-1][0]
+                    x = x/2
+                    x = Sample_point_tuple [i-1][0] + x
+                    y = Sample_point_tuple[i][1] - Sample_point_tuple[i-1][1]
+                    y = y/2
+                    y = Sample_point_tuple[i][1] - Sample_point_tuple[i-1][1]
+                    new_interpolated_point = (x, y, ts)
+                    interpolated_sample_point = interpolated_sample_point + (new_interpolated_point,)
+        print(interpolated_sample_point)
+        # print(len(interpolated_sample_point))
+
+
+          #      loc_of_sp = i
+           #     num_interpolation = int(delta_ts / time_parameter)
+            #    a = len(i)
+             #   for b in range(1,a)
+              #      if num_interpolation == b:
+               #         distance_of_ts[b] = delta_ts/(b+1)
+                #        distance_of_second_ts = distance_of_first_ts * 2
+                 #   elif num_interpolation == i+1:
+                  #      distance_of_first_ts = delta_ts/
+                   #     distance_of_second_ts = distance_of_first_ts * 2
 
 
 
@@ -98,8 +140,8 @@ def test():
 # Multi-threading
 def main():
     thread1 = threading.Thread(target=call_gaze)
-    thread2 = threading.Thread(target=test)
-    thread3 = threading.Thread(target=sp)
+    thread2 = threading.Thread(target=sp)
+    thread3 = threading.Thread(target=interpolation)
     # Will execute both in parallel
     thread1.start()
     thread2.start()
